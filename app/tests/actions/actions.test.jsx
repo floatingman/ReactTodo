94a9defb41de1bf368e5b5c7ba147c67619caa82
Daniel Newman <dwnewman78@gmail.com>
2016-06-04 21:05:19 -0500

    started adding reducers and actions

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,24 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+  it('should generate search text action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Some search text'
+    };
+    var res = actions.setSearchText(action.searchText);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate add todo action', () => {
+    var action = {
+      type: 'ADD_TODO',
+      text: 'Some todo text'
+    };
+    var res actions.addTodo(action.text);
+
+    expect(res).toEqual(action);
+  });
+});
